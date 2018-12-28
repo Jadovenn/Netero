f2c069748625db34f9b4bb0e475455149ee79d4a
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2018-12-28 20:35:40 +0100

    add createEntity test

--- Test/ECS/ecs_world.cpp
+++ Test/ECS/ecs_world.cpp
@@ -3,6 +3,23 @@
  * see LICENCE.txt
  */
 
+#include "../../Headers/netero/netero.hpp"
+
+netero::ecs::World	world;
+
+int	createEntity() {
+	netero::ecs::Entity	first = world.createEntity();
+	if (!first.valid())
+		return 1;
+	netero::ecs::Entity second = world.createEntity("second");
+	if (!second.valid())
+		return 2;
+	return 0;
+}
+
 int	main() {
+	int ret = createEntity();
+	if (ret != 0)
+		return ret;
 	return 0;
 }
