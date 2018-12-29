7fddaa4fcd0f3943f5ff4121fdc495bd15494e43
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2018-12-29 16:42:28 +0100

    update entity test

--- Test/ECS/ecs_entity.cpp
+++ Test/ECS/ecs_entity.cpp
@@ -11,12 +11,32 @@ netero::ecs::World	world;
 int	entityLocalScope() {
 	std::cout << "ecs_entity: Create entity first" << std::endl;
 	netero::ecs::Entity	first = world.createEntity();
+	std::cout << "ecs_entity: Create entity second" << std::endl;
+	netero::ecs::Entity	second = world.createEntity();
+	std::cout << "ecs_entity: Enable entity second" << std::endl;
+	second.enable();
 	std::cout << "ecs_entity: entity killed" << std::endl;
 	first.kill();
 	if (first.valid())
 		return 1;
-	if (world.size() != 0)
+	std::cout << "ecs_entity: update world" << std::endl;
+	world.update();
+	auto stat = world.getStatistic();
+	if (stat.size != 1)
 		return 2;
+	if (stat.garbadgeSize != 1)
+		return 3;
+	if (stat.activeEntities != 1 || stat.unactiveEntities != 0)
+		return 4;
+	std::cout << "ecs_entity: update world" << std::endl;
+	world.update();
+	stat = world.getStatistic();
+	if (stat.size != 1)
+		return 5;
+	if (stat.garbadgeSize != 0)
+		return 6;
+	if (stat.activeEntities != 1 || stat.unactiveEntities != 0)
+		return 7;
 	return 0;
 }
 
