1b3684ee8bfcf7cc7660be239b4561bd45da9b2a
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2018-12-30 00:29:14 +0100

    add test for getComponent, deleteComponent

--- Test/ECS/ecs_component.cpp
+++ Test/ECS/ecs_component.cpp
@@ -10,10 +10,15 @@ netero::ecs::World	world;
 
 struct Position : public netero::ecs::Component {
 	Position(int x = 0, int y = 0)
-		:	x(y), y(y)
+		:	x(x), y(y)
 	{}
 	int		x;
 	int		y;
+};
+
+struct Id : public netero::ecs::Component {
+	std::string		firstName;
+	std::string		lastName;
 };
 
 int	component() {
@@ -21,11 +26,30 @@ int	component() {
 	netero::ecs::Entity	first = world.createEntity();
 	std::cout << "ecs_entity: Add Component Position to Entity first" << std::endl;
 	auto &position = first->addComponent<Position>(10, 9);
+	auto &id = first->addComponent<Id>();
+	id.firstName = "test";
 	try {
 		first->addComponent<Position>();
 		return 1;
 	}
 	catch (...) {
+	}
+	try {
+		auto &pos = first->getComponent<Position>();
+		auto &i = first->getComponent<Id>();
+		if (pos.x != 10)
+			return 2;
+		if (i.firstName != id.firstName)
+			return 3;
+	}
+	catch (...) {
+		return 4;
+	}
+	try {
+		first->deleteComponent<Id>();
+	}
+	catch (...) {
+		return 5;
 	}
 	return 0;
 }
