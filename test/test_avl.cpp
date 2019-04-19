c007d7991bfe56b57ed448f719c70fe6b35a1191
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-04-18 22:55:54 -0700

    update netro::avl test

--- test/test_avl.cpp
+++ test/test_avl.cpp
@@ -6,9 +6,10 @@
 // add debug info to netero, enable display method
 #define NETERO_DEBUG
 
+#include <memory>
 #include "netero/avl.hpp"
 
-int 	main() {
+int     basic_test() {
 	netero::avl<int>		avl_tree;
 
 	avl_tree.add(5);
@@ -23,20 +24,43 @@ int 	main() {
 		return 1;
 	if (avl_tree.search(23))
 		return 1;
-	avl_tree.display();
 	std::cout << "Range base for-loop compatibility:" << std::endl;
 	for (const auto &e: avl_tree) { // perform in-order traversal
 		std::cout << e << std::endl;
 	}
-	avl_tree.display();
-	std::cout << "Delete item 1" << std::endl;
 	avl_tree.remove(1);
-	avl_tree.display();
-	std::cout << "Delete item 2" << std::endl;
+    if (avl_tree.search(1))
+        return 1;
 	avl_tree.remove(2);
-	avl_tree.display();
-	std::cout << "Delete item 12" << std::endl;
 	avl_tree.remove(12);
 	avl_tree.display();
+	return 0;
+}
+
+int    ctor_and_operator_test() {
+    netero::avl<int>         tree1;
+    tree1.add(1);tree1.add(2);tree1.add(3);tree1.add(4);
+    netero::avl<int>          tree2(tree1);
+    if (!tree2.search(1))
+        return 1;
+    std::shared_ptr<netero::avl<int>>   tree3 = std::make_shared<netero::avl<int>>();
+    tree3->add(42);tree3->add(45);
+    tree2 = *tree3;
+    if (tree2.search(1))
+        return 1;
+    if (!tree2.search(42))
+        return 1;
+    tree1 = std::move(tree2);
+    if (tree2.search(42))
+        return 1;
+    tree3.reset();
+    return 0;
+}
+
+int 	main() {
+    if (basic_test() != 0)
+        return 1;
+    if (ctor_and_operator_test() != 0)
+        return 1;
 	return 0;
 }
