e1d8f67f1b7acaa9186b5811d5d9fb459bd47606
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-12-23 11:19:51 +0100

    add interWith test for netero set

--- test/test_set.cpp
+++ test/test_set.cpp
@@ -10,10 +10,17 @@ int 	main() {
 	netero::set<int>	b{6, 7, 8};
 	std::set<int>		c{4, 5, 6};
 	netero::set<int>	d(c);
+	netero::set<int>	e{5, 10};
 
 	if (b.isSubsetOf(c)) // NOT OK
 		return 1;
 	if (!b.isSubsetOf(a)) // OK
 		return 1;
+	if (e.isSubsetOf(a)) {
+		return 1;
+	}
+	if (!e.interWith(a)) {
+		return 1;
+	}
 	return 0;
 }
