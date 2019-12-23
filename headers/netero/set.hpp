186b84ee93f1bb5397dc32f44b515548d94e63c2
Jean-Adrien Domage <jadomage-prestataire@vente-privee.com>
2019-12-23 11:29:34 +0100

    improve isSubsetOf from netero::set

--- headers/netero/set.hpp
+++ headers/netero/set.hpp
@@ -34,28 +34,29 @@ namespace netero {
 		 * @brief check if the actual set is a subset of the given set
 		 * @note the given set does not need to be a netero::set
 		 * @param other - set to compare
-		 * @return true is this is an subset, false otherwise
+		 * @return true if this is an subset, false otherwise
 		 */
 		bool	isSubsetOf(const std::set<__Type> &other) const {
 			iterator		it_this = this->begin();
 			iterator		it_this_end = this->end();
-			iterator		it_other = other.begin();
-			iterator		it_other_end = other.end();
 			if (other.size() == 0)
 				return false;
 			while (it_this != it_this_end) {
-				while (*it_this != *it_other) {
-					it_other++;
-					if (it_other == it_other_end)
-						return false;
+				auto it = other.find(*it_this);
+				if (it == other.end()) {
+					return false;
 				}
-				it_this++;
+				++it_this;
 			}
 			return true;
-		} // O(n) = n, where n is size of other
-		// TODO: union -> return new set wich is union of two other set
-		// TODO: inter
+		} // O(n, m) = n log(m), where n is size of this and m size of other
 
+		/**
+		 * @brief check if two sets has an intersection
+		 * @note the given set does not need to be a netero::set
+		 * @param other - set to compare
+		 * @return true if their is a set which is a subset of both sets, false otherwise
+		 */
 		bool	interWith(const std::set<__Type>& other) {
 			iterator		it_this = this->begin();
 			iterator		it_this_end = this->end();
@@ -70,6 +71,6 @@ namespace netero {
 				++it_this;
 			}
 			return false;
-		}
+		} // O(n, m) = n log(m), where n is size of this and m size of other
 	};
 }
