516ff986d7fc9fcfab27989270e601746458edeb
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-03-20 11:13:34 +0100

    add AudioStream interface

--- headers/netero/audio/audio.hpp
+++ headers/netero/audio/audio.hpp
@@ -14,5 +14,21 @@ namespace netero::audio {
 		ERR_MISSING_CALLBACK = 1,
 		ERR_NATIVE = 2,
 	};
+
+	struct WaveFormat {
+		unsigned	channels;
+		unsigned	nBlocks;
+		unsigned	samplePerSecond;
+	};
+
+	class AudioStream {
+	public:
+		virtual void setFormat(WaveFormat &) = 0;
+		virtual double render(int delta, int channel) = 0;
+
+		virtual void	play() = 0;
+		virtual void	pause() = 0;
+		virtual void	stop() = 0;
+	};
 }
 
