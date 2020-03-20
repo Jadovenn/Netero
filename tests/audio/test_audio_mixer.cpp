d3cc1e088b385930f4f4743017205b52a8497778
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-03-20 14:31:31 +0100

    rename e_minor to a_minor, change amplitude to adequate value

--- tests/audio/test_audio_mixer.cpp
+++ tests/audio/test_audio_mixer.cpp
@@ -11,15 +11,15 @@
 
 int main() {
     netero::audio::device& audio_device = netero::audio::device::GetAudioDevice();
-    netero::audio::signals::sinusoid    e_minor(1, 440, 0);
+    netero::audio::signals::sinusoid    a_minor(0.006, 440, 0);
 
-    audio_device.connect(&e_minor);
-    e_minor.play();
+    audio_device.connect(&a_minor);
+    a_minor.play();
     std::chrono::time_point start = std::chrono::system_clock::now();
     while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count() < 3) {
         std::this_thread::yield();
     }
-    e_minor.stop();
-    audio_device.disconnect(&e_minor);
+    a_minor.stop();
+    audio_device.disconnect(&a_minor);
     return 0;
 }
