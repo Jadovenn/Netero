ce9fdd1c210b5fb103d37cb6aa3cf6d91ca9d054
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-03-23 17:07:31 +0100

    update mixer test, use a floating value for the signale

--- tests/audio/test_audio_mixer.cpp
+++ tests/audio/test_audio_mixer.cpp
@@ -10,15 +10,15 @@
 #include <thread>
 
 int main() {
-    netero::audio::signals::sinusoid    _la(0.01, 49, 0);
-    netero::audio::signals::sinusoid    _sol(0.01, 440, 0);
-    _la.play();
-    _sol.play();
+    netero::audio::signals::sinusoid    _mi(0.01, 329.63, 0);
+    //netero::audio::signals::sinusoid    _sol(0.01, 440, 0);
+    _mi.play();
+    //_sol.play();
     std::chrono::time_point start = std::chrono::system_clock::now();
     while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count() < 10) {
         std::this_thread::yield();
     }
-    _la.stop();
-    _sol.stop();
+    _mi.stop();
+    //_sol.stop();
     return 0;
 }
