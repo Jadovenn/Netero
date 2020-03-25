7a0ca618bf2e002f77364f639d748c79590b3e2e
Jean-Adrien Domage <jadomage@vente-privee.com>
2020-03-25 12:21:42 +0100

    fix all dependency issue with cmake/ install rules

--- cmake/neteroConfig.cmake
+++ cmake/neteroConfig.cmake
@@ -2,5 +2,5 @@ get_filename_component(netero_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
 if(NOT TARGET Netero::netero)
-    include("${netero_CMAKE_DIR}/neteroTargets.cmake")
+    include("${netero_CMAKE_DIR}/cmake/netero/neteroTargets.cmake")
 endif()
