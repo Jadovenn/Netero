06e03f5bd6acf43a5044609ede302d27c9fa3181
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-04-02 13:36:35 +0200

    add circular buffer test and audio recorder test

---
+++ tests/audio/test_audio_waveRecorder.cpp
@@ -0,0 +1,63 @@
+/**
+ * Netero sources under BSD-3-Clause
+ * see LICENCE.txt
+ */
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <netero/observer/slots.hpp>
+#include <netero/audio/audio.hpp>
+#include <netero/audio/engine.hpp>
+#include <netero/audio/waveFormat.hpp>
+
+void    errorCallback(const std::string& error) {
+    std::cout << "[ERROR] " << error << std::endl;
+}
+
+int    select_recording_device(netero::audio::engine& engine) {
+    auto devices = engine.getOutDevices();
+    auto device_in = engine.getInDevices();
+    devices.insert(devices.end(), device_in.begin(), device_in.end());
+    int idx = 0;
+    for (auto& device : devices) {
+        std::cout << idx << ": " << device.name << std::endl;
+        idx += 1;
+    }
+    std::cout << "Choose a device: ";
+    int device_idx;
+    std::cin >> device_idx;
+    if (device_idx < 0 || device_idx > idx) {
+        std::cout << "Choosen idx out of bound!" << std::endl;
+        return 1;
+    }
+    netero::audio::RtCode code = engine.setOutputDevice(devices[device_idx]);
+    if (code != netero::audio::RtCode::OK) {
+        std::cout << "Error while setting up the device: " << devices[device_idx].name << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+
+int     main() {
+    netero::audio::engine                   audio_engine;
+    netero::audio::waveRecorder             wave_recorder("test_record.wav");
+    netero::slot<void(const std::string&)>  captureErrorSlot(&errorCallback);
+
+    if (select_recording_device(audio_engine) != 0) {
+        return 1;
+    }
+    audio_engine.captureErrorSig.connect(&captureErrorSlot);
+    wave_recorder.connect(audio_engine);
+    audio_engine.startCapture();
+    wave_recorder.record();
+    std::chrono::time_point start = std::chrono::system_clock::now();
+    while (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start).count() < 10) {
+        std::this_thread::yield();
+    }
+    wave_recorder.stop();
+    audio_engine.stopCapture();
+    return 0;
+}
+
---
+++ tests/test_circular_buffer.cpp
@@ -0,0 +1,72 @@
+/**
+ * Netero sources under BSD-3-Clause
+ * see LICENCE.txt
+ */
+
+#include <netero/circular_buffer.hpp>
+
+int test_full_fill_buffer() {
+    int buf[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    netero::shared_circular_buffer<int>  buffer(10);
+    buffer.write(buf, 3);
+    buffer.write(buf + 3, 3);
+    buffer.write(buf + 6, 4);
+    if (buffer.write(buf, 3) != 0)
+        return 1;
+    return 0;
+}
+
+int test_full_read() {
+    int buf[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int outBuf[10] = {};
+    netero::shared_circular_buffer<int>  buffer(10);
+    buffer.write(buf, 10);
+    buffer.read(outBuf, 3);
+    buffer.read(outBuf + 3, 4);
+    buffer.read(outBuf + 7, 3);
+    if (buffer.read(outBuf, 10) != 0)
+        return 1;
+    return 0;
+}
+
+int test_faster_read() {
+    int buf[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int outBuf[10] = {};
+    netero::shared_circular_buffer<int>  buffer(10);
+    if (buffer.write(buf, 3) != 3) {
+        return 1;
+    }
+    if (buffer.read(outBuf, 5) != 3) {
+        return 1;
+    }
+    if (buffer.write(buf+3, 3) != 3) {
+        return 1;
+    }
+    if (buffer.read(outBuf+3, 5) != 3) {
+        return 1;
+    }
+    if (buffer.write(buf+6, 3) != 3) {
+        return 1;
+    }
+    if (buffer.read(outBuf+6, 5) != 3) {
+        return 1;
+    }
+    if (buffer.write(buf+9, 3) != 1) {
+        return 1;
+    }
+    if (buffer.read(outBuf+9, 5) != 1) {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int result = test_full_fill_buffer();
+    if (result) { return result; }
+    result = test_full_read();
+    if (result) { return result; }
+    result = test_faster_read();
+    if (result) { return result; }
+    return 0;
+}
+
