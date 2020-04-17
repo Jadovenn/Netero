711cde1b96f144dd574654108327ce7811795f7c
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-04-18 00:30:10 +0200

    graphics/ initial commit, setup vulkan

---
+++ modules/graphics/renderContext/Vulkan.hpp
@@ -0,0 +1,13 @@
+/**
+ * Netero sources under BSD-3-Clause
+ * see LICENSE.txt
+ */
+
+#pragma once
+
+/**
+ * @file Vulkan.hpp
+ * @brief Private header, that include GLFW and vulkan header.
+ */
+
+
