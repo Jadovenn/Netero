17e51978c87c898f46b7c9f40e7d06055239c10e
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-04-25 15:46:50 +0200

    graphics/ add vulkan debug layers extentions loader

---
+++ modules/graphics/vulkan/vulkanExtention.cpp
@@ -0,0 +1,37 @@
+/**
+ * Netero sources under BSD-3-Clause
+ * see LICENSE.txt
+ */
+
+#include "vulkan.hpp"
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    const auto function = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT"));
+    if (function != nullptr) {
+        return function(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    const auto function = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT"));
+    if (function != nullptr) {
+        function(instance, debugMessenger, pAllocator);
+    }
+}
+
+VkResult CreateDebugReportEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* createInfo, const VkAllocationCallbacks* allocator, VkDebugReportCallbackEXT* debugReportCallback) {
+    const auto function = reinterpret_cast<PFN_vkCreateDebugReportCallbackEXT>(vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT"));
+    if (function != nullptr) {
+        return function(instance, createInfo, allocator, debugReportCallback);
+    }
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+}
+
+void DestroyDebugReportEXT(VkInstance instance, VkDebugReportCallbackEXT debugReportCallback, const VkAllocationCallbacks* pAllocator) {
+    const auto function = reinterpret_cast<PFN_vkDestroyDebugReportCallbackEXT>(vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT"));
+    if (function != nullptr) {
+        function(instance, debugReportCallback, pAllocator);
+    }
+}
+
