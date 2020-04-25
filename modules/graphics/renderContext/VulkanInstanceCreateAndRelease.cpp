205b3d59969f03aae7d45f4f53c3dad59636e4d0
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-04-25 15:46:50 +0200

    graphics/ add vulkan debug layers extentions loader

--- modules/graphics/renderContext/VulkanInstanceCreateAndRelease.cpp
+++ modules/graphics/renderContext/VulkanInstanceCreateAndRelease.cpp
@@ -3,7 +3,7 @@
  * see LICENSE.txt
  */
 
-#define GLFW_INCLUDE_VULKAN
+#include "vulkan.hpp"
 #include <GLFW/glfw3.h>
 
 #include <algorithm>
@@ -35,36 +35,6 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugReportCallback(VkFlags msgFlags,
     void* pUserData) {
     std::cerr << pMsg << std::endl;
     return VK_FALSE;
-}
-
-VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
-    const auto function = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT"));
-    if (function != nullptr) {
-        return function(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    }
-    return VK_ERROR_EXTENSION_NOT_PRESENT;
-}
-
-void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
-    const auto function = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT"));
-    if (function != nullptr) {
-        function(instance, debugMessenger, pAllocator);
-    }
-}
-
-VkResult CreateDebugReportEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* createInfo, const VkAllocationCallbacks* allocator, VkDebugReportCallbackEXT* debugReportCallback) {
-    const auto function = reinterpret_cast<PFN_vkCreateDebugReportCallbackEXT>(vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT"));
-    if (function != nullptr) {
-        return function(instance, createInfo, allocator, debugReportCallback);
-    }
-    return VK_ERROR_EXTENSION_NOT_PRESENT;
-}
-
-void DestroyDebugReportEXT(VkInstance instance, VkDebugReportCallbackEXT debugReportCallback, const VkAllocationCallbacks* pAllocator) {
-    const auto function = reinterpret_cast<PFN_vkDestroyDebugReportCallbackEXT>(vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT"));
-    if (function != nullptr) {
-        function(instance, debugReportCallback, pAllocator);
-    }
 }
 
 void netero::graphics::RenderContext::impl::createVulkanContext() {
