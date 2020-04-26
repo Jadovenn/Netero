39ec741baf06c67a6ceb1cd3eeea43f455d2e39a
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-04-26 12:22:20 +0200

    tools/ add device scoring log in graphicExample tool

--- tools/graphics_example/sources/main.cpp
+++ tools/graphics_example/sources/main.cpp
@@ -6,24 +6,30 @@
 #include <iostream>
 #include <netero/graphics/window.hpp>
 #include <netero/graphics/renderContext.hpp>
+#include <netero/graphics/physicalDevice.hpp>
 
-int	main() {
-	try {
-		const netero::graphics::window	m_window(800, 600, "GraphicsExample");
-		const netero::graphics::RenderContext m_context("GraphicsExample", true);
-		m_window.initialize();
-		m_context.initialize();
-		m_window.runLoop();
-		m_context.release();
-	}
-	catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
-	catch(...) {
-		std::cerr << "Fatal error." << std::endl;
-		return 1;
-	}
-	return 0;
+int    main() {
+    try {
+        const netero::graphics::window    m_window(800, 600, "GraphicsExample");
+        const netero::graphics::RenderContext m_context("GraphicsExample", true);
+        m_window.initialize();
+        m_context.initialize();
+        const auto physicalDevice = netero::graphics::PhysicalDevice::Instance();
+        const auto physicalDevices = physicalDevice->getDevices();
+        for (const auto& device: physicalDevices) {
+            std::cout << "Score: " << device.first << " name: " << device.second << std::endl;
+        }
+        m_window.runLoop();
+        m_context.release();
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Fatal error." << std::endl;
+        return 1;
+    }
+    return 0;
 }
 
