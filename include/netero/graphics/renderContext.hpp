844c73d03a35c24dc952bd522f04d07180efea60
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-04-26 15:30:45 +0200

    graphics/ add IS_DEBUG macro

--- include/netero/graphics/renderContext.hpp
+++ include/netero/graphics/renderContext.hpp
@@ -8,11 +8,17 @@
 #include <string>
 #include <memory>
 
+#if defined(NDEBUG)
+#	define	IS_DEBUG	true
+#else
+#	define	IS_DEBUG	false
+#endif
+
 namespace netero::graphics {
 
 	class RenderContext {
 	public:
-		explicit RenderContext(const std::string &appName, bool enableValidationLayers = false);
+		explicit RenderContext(const std::string &appName, bool enableValidationLayers = IS_DEBUG);
 		RenderContext(const RenderContext&) = delete;
 		RenderContext(RenderContext&&) = delete;
 		RenderContext& operator=(const RenderContext&) = delete;
