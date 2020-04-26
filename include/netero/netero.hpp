91f7ffa047a1c36b5fb9453fb081b7d0270bf9ed
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-04-26 12:19:56 +0200

    core/ add likely/unlikely macro

--- include/netero/netero.hpp
+++ include/netero/netero.hpp
@@ -28,3 +28,10 @@ namespace netero {
 	}
 }
 
+#if defined __GNUC__ || defined __clang__
+#   define likely(x)   __builtin_expect(!!(x), 1)
+#   define unlikely(x) __builtin_expect(!!(x), 0)
+#else
+#   define likely(x)   (x)
+#   define unlikely(x) (x)
+#endif
