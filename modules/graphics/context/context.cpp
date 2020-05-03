ceb9f589f80f7c0bac449fa21eacd518f064ded3
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-05-03 20:28:44 +0200

    graphics/ add image views creation

--- modules/graphics/context/context.cpp
+++ modules/graphics/context/context.cpp
@@ -56,12 +56,15 @@ namespace netero::graphics {
         if (swapChainDetails.formats.empty() || swapChainDetails.presentMode.empty()) {
             throw std::runtime_error("The device (" + vkUtils::getDeviceName(this->_physicalDevice) + ") is as no suitable swap chain.");
         }
-        // Create logical device
         this->createLogicalDevice(this->_physicalDevice);
         this->createSwapchain();
+        this->createImageViews();
     }
 
     Context::~Context() {
+        for (auto imageView: this->_swapchainImageViews) {
+            vkDestroyImageView(this->_logicalDevice, imageView, nullptr);
+        }
         vkDestroySwapchainKHR(this->_logicalDevice, this->_swapchain, nullptr);
         vkDestroySurfaceKHR(this->_vulkanInstance, this->_surface, nullptr);
         vkDestroyDevice(this->_logicalDevice, nullptr);
@@ -158,6 +161,36 @@ namespace netero::graphics {
         this->_swapchainImageFormat = surfaceFormat.format;
         this->_swapchainExtent = this->_swapchainExtent;
     }
+
+    void Context::createImageViews() {
+        VkImageViewCreateInfo   createInfo = {};
+
+        createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        createInfo.format = this->_swapchainImageFormat;
+        createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        createInfo.subresourceRange.baseMipLevel = 0;
+        createInfo.subresourceRange.levelCount = 1;
+        createInfo.subresourceRange.baseArrayLayer = 0;
+        createInfo.subresourceRange.layerCount = 1;
+
+        this->_swapchainImageViews.resize(this->_swapchainImage.size());
+        for (unsigned idx = 0; idx < this->_swapchainImageViews.size(); idx++) {
+            createInfo.image = this->_swapchainImage[idx];
+            const VkResult result = vkCreateImageView(this->_logicalDevice,
+                &createInfo,
+                nullptr,
+                &this->_swapchainImageViews[idx]);
+            if (result != VK_SUCCESS) {
+                throw std::runtime_error("Failed to create image views.");
+            }
+        }
+    }
+
 
 }
 
