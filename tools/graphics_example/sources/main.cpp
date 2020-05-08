1662d6be7587b11dae0335a5240c6054807982e9
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-05-08 21:23:56 +0200

    tools/graphics/ retrieve shaders path using netero::os::getBundlePath

--- tools/graphics_example/sources/main.cpp
+++ tools/graphics_example/sources/main.cpp
@@ -4,17 +4,22 @@
  */
 
 #include <iostream>
+#include <netero/os.hpp>
 #include <netero/graphics/application.hpp>
 #include <netero/graphics/context.hpp>
 
+const std::string g_triangleVertices_path = netero::os::getBundlePath() + "/shaders/triangleVertices.spv";
+const std::string g_triangleFragment_path = netero::os::getBundlePath() + "/shaders/triangleFragment.spv";
+
 int    main() {
     int rtCode = 0;
+    std::cout << netero::os::getBundlePath() << std::endl;
     try {
         auto* app = netero::graphics::Application::Initialize("GraphicsExample");
         auto* context = app->newWindowedContext(800, 600, netero::graphics::WindowMode::FIX);
         std::cout << "Current Device:" << context->getCurrentPhysicalDeviceName() << std::endl;
-        context->loadShader("./shaders/triangleVertices.spv", netero::graphics::ShaderStage::VERTEX);
-        context->loadShader("./shaders/triangleFragment.spv", netero::graphics::ShaderStage::FRAGMENT);
+        context->loadShader(g_triangleVertices_path, netero::graphics::ShaderStage::VERTEX);
+        context->loadShader(g_triangleFragment_path, netero::graphics::ShaderStage::FRAGMENT);
         context->run();
     }
     catch (const std::exception &e) {
