5ef2d4f153d3886f53eb656bd2cb0aa6e21d2752
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-05-24 00:47:27 +0200

    graphics/ add TransferBuffer utils function

--- modules/graphics/utils/vkMemory.cpp
+++ modules/graphics/utils/vkMemory.cpp
@@ -49,5 +49,35 @@ namespace vkUtils {
         return memory;
     }
 
+    void        TransferBuffer(netero::graphics::Device* device, VkBuffer source, VkBuffer destination, VkDeviceSize size) {
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = device->transferCommandPool;
+        allocInfo.commandBufferCount = 1;
+
+        VkCommandBuffer commandBuffer;
+        vkAllocateCommandBuffers(device->logicalDevice, &allocInfo, &commandBuffer);
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+        vkBeginCommandBuffer(commandBuffer, &beginInfo);
+        VkBufferCopy copyRegion{};
+        copyRegion.srcOffset = 0;
+        copyRegion.dstOffset = 0;
+        copyRegion.size = size;
+        vkCmdCopyBuffer(commandBuffer, source, destination, 1, &copyRegion);
+        vkEndCommandBuffer(commandBuffer);
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+
+        vkQueueSubmit(device->transferQueue, 1, &submitInfo, nullptr);
+        vkQueueWaitIdle(device->transferQueue);
+        vkFreeCommandBuffers(device->logicalDevice, device->transferCommandPool, 1, &commandBuffer);
+    }
+
 }
 
