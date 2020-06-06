11398679245cb94664142e5e884c403c9e987564
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-06-06 16:04:31 +0200

    memcheck/ update test_allocator and test_avl

--- tests/memcheck/test_allocator.cpp
+++ tests/memcheck/test_allocator.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <netero/avl.hpp>
 #include <netero/memcheck/allocator.hpp>
 
 class test {
@@ -36,9 +37,44 @@ void on_class_test() {
     std::cout << "on_class_test -- end" << std::endl;
 }
 
+void rebind_test() {
+    std::cout << "rebind_test -- start" << std::endl;
+    netero::memck::Allocator<int>  allocator;
+    netero::memck::Allocator<int>::rebind<char>::other  rebindAllocator;
+    auto* integer = std::allocator_traits<netero::memck::Allocator<int>>::allocate(allocator, 1);
+    auto* character = std::allocator_traits<netero::memck::Allocator<int>::rebind<char>::other>::allocate(rebindAllocator, 1);
+    *integer = 42;
+    *character = 'A';
+    std::allocator_traits<netero::memck::Allocator<int>::rebind<char>::other>::deallocate(rebindAllocator, character, 1);
+    std::allocator_traits<netero::memck::Allocator<int>>::deallocate(allocator, integer, 1);
+    std::cout << "rebind_test -- end" << std::endl;
+}
+
+void avl_test() {
+    std::cout << "avl_test -- start" << std::endl;
+    netero::avl<int, netero::memck::Allocator<int>> memcheckAvl;
+    memcheckAvl.add(5);
+    memcheckAvl.add(2);
+    memcheckAvl.add(1);
+    memcheckAvl.add(12);
+    memcheckAvl.add(24);
+    memcheckAvl.add(8);
+    memcheckAvl.add(3);
+    memcheckAvl.add(4);
+    memcheckAvl.search(1);
+    memcheckAvl.search(23);
+    memcheckAvl.remove(1);
+    memcheckAvl.search(1);
+    memcheckAvl.remove(2);
+    memcheckAvl.remove(12);
+    std::cout << "avl_test -- start" << std::endl;
+}
+
 int main() {
     stl_basic_container_test();
     on_class_test();
+    rebind_test();
+    avl_test();
     return 0;
 }
 
--- tests/test_avl.cpp
+++ tests/test_avl.cpp
@@ -1,40 +1,41 @@
 /**
  * Netero sources under BSD-3-Clause
- * see LICENCE.txt
+ * see LICENSE.txt
  */
 
 // add debug info to netero, enable display method
 #define NETERO_DEBUG
 
+#include <iostream>
 #include <memory>
 #include "netero/avl.hpp"
 
-int     basic_test() {
-	netero::avl<int>		avl_tree;
+int basic_test() {
+    netero::avl<int>        avl_tree;
 
-	avl_tree.add(5);
-	avl_tree.add(2);
-	avl_tree.add(1);
-	avl_tree.add(12);
-	avl_tree.add(24);
-	avl_tree.add(8);
-	avl_tree.add(3);
-	avl_tree.add(4);
-	if (!avl_tree.search(1))
-		return 1;
-	if (avl_tree.search(23))
-		return 1;
-	std::cout << "Range base for-loop compatibility:" << std::endl;
-	for (const auto &e: avl_tree) { // perform in-order traversal
-		std::cout << e << std::endl;
-	}
-	avl_tree.remove(1);
+    avl_tree.add(5);
+    avl_tree.add(2);
+    avl_tree.add(1);
+    avl_tree.add(12);
+    avl_tree.add(24);
+    avl_tree.add(8);
+    avl_tree.add(3);
+    avl_tree.add(4);
+    if (!avl_tree.search(1))
+        return 1;
+    if (avl_tree.search(23))
+        return 1;
+    std::cout << "Range base for-loop compatibility:" << std::endl;
+    for (const auto &e: avl_tree) { // perform in-order traversal
+        std::cout << e << std::endl;
+    }
+    avl_tree.remove(1);
     if (avl_tree.search(1))
         return 1;
-	avl_tree.remove(2);
-	avl_tree.remove(12);
-	avl_tree.display();
-	return 0;
+    avl_tree.remove(2);
+    avl_tree.remove(12);
+    avl_tree.display();
+    return 0;
 }
 
 int    ctor_and_operator_test() {
@@ -57,10 +58,10 @@ int    ctor_and_operator_test() {
     return 0;
 }
 
-int 	main() {
+int     main() {
     if (basic_test() != 0)
         return 1;
     if (ctor_and_operator_test() != 0)
         return 1;
-	return 0;
+    return 0;
 }
