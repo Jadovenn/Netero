50a8bc055f1acac8cfa47cf3cac1c63c8242c3ca
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-06-18 22:25:02 +0200

    graphics/descriptor/ finish minimal Descriptor container

--- include/netero/graphics/descriptor.hpp
+++ include/netero/graphics/descriptor.hpp
@@ -11,28 +11,45 @@
 
 namespace netero::graphics {
 
-    class Descriptor {
+    class DescriptorSets {
     public:
-        Descriptor(Device*, uint32_t);
-        ~Descriptor();
 
-        void    setDescriptorType(VkDescriptorType);
+        static const int WHOLE_SIZE;
+
+        enum class Error {
+            SUCCESS,
+            BAD_DESCRIPTOR_TYPE,
+            EMPTY_DESCRIPTOR_SET,
+            INDEX_OUT_OF_BOUND
+        };
+
+        DescriptorSets(Device* device, uint32_t descriptorSetCount);
+        ~DescriptorSets();
+
+        void    setDescriptorSetType(VkDescriptorType);
         void    setShaderStage(VkShaderStageFlags);
         void    setBinding(uint32_t);
+        [[nodiscard]] VkDescriptorSetLayout getLayout() const;
+        [[nodiscard]] VkDescriptorSet at(uint32_t) const;
 
         void    build();
         void    rebuild();
         void    release();
-        void    update();
+
+        Error    write(int, VkImageView, VkSampler);
+        Error    write(int, VkBuffer, VkDeviceSize, VkDeviceSize);
 
     private:
-        void    _createDescriptorPool();
-        void    _createDescriptorLayout();
+        void    _createDescriptorSetPool();
+        void    _createDescriptorSetLayout();
         void    _createDescriptorSets();
 
-        // Node Related
+        Error   _write(int, VkDescriptorBufferInfo*, VkDescriptorImageInfo*);
+
+        // Instance Related
         Device*     _device = nullptr;
-        uint32_t    _frameCount = 0;
+        uint32_t    _descriptorCount = 0;
+        const uint32_t    _descriptorCountPerSet = 1; /**< Number of descriptor(s) in a set. Might be configurable in the future. */
         VkDescriptorType    _descriptorType = VK_DESCRIPTOR_TYPE_MAX_ENUM;
         VkShaderStageFlags  _shaderStage = VK_SHADER_STAGE_FLAG_BITS_MAX_ENUM;
         uint32_t            _binding = 0;
--- modules/graphics/descriptor.cpp
+++ modules/graphics/descriptor.cpp
@@ -8,61 +8,74 @@
 
 namespace netero::graphics {
 
-    Descriptor::Descriptor(Device* device, uint32_t frameCount)
+    const int DescriptorSets::WHOLE_SIZE = -1;
+
+    DescriptorSets::DescriptorSets(Device* device, uint32_t descriptorSetCount)
         :   _device(device),
-            _frameCount(frameCount)
+            _descriptorCount(descriptorSetCount)
     {}
 
-    Descriptor::~Descriptor() {}
+    DescriptorSets::~DescriptorSets() {
+        this->release();
+        vkDestroyDescriptorSetLayout(this->_device->logicalDevice, this->_descriptorSetLayout, nullptr);
+    }
 
-    void Descriptor::setDescriptorType(VkDescriptorType descriptorType) {
+    void DescriptorSets::setDescriptorSetType(VkDescriptorType descriptorType) {
         this->_descriptorType = descriptorType;
     }
 
-    void Descriptor::setShaderStage(VkShaderStageFlags shaderStage) {
+    void DescriptorSets::setShaderStage(VkShaderStageFlags shaderStage) {
         this->_shaderStage = shaderStage;
     }
 
-    void Descriptor::setBinding(uint32_t binding) {
+    void DescriptorSets::setBinding(uint32_t binding) {
         this->_binding = binding;
     }
 
-    void Descriptor::build() {
-        this->_createDescriptorPool();
+    void DescriptorSets::build() {
+        this->_createDescriptorSetPool();
         this->_createDescriptorSets();
-        this->_createDescriptorLayout();
+        this->_createDescriptorSetLayout();
     }
 
-    void Descriptor::rebuild() {
+    void DescriptorSets::rebuild() {
+        this->_createDescriptorSetPool();
+        this->_createDescriptorSets();
     }
 
-    void Descriptor::release() {
+    VkDescriptorSetLayout DescriptorSets::getLayout() const {
+        return this->_descriptorSetLayout;
     }
 
-    // Noop but will be needed
-    void Descriptor::update() {}
+    VkDescriptorSet DescriptorSets::at(uint32_t index) const {
+        if (index >= this->_descriptorSets.size()) {
+            return nullptr;
+        }
+        return this->_descriptorSets[index];
+    }
 
-    // TODO: Move DescriptorPool in Pipeline
-    // DescriptorPool could be hold by the pipeline a created
-    // before any descriptor as we may know how much of them we need
-    void Descriptor::_createDescriptorPool() {
+    void DescriptorSets::release() {
+        vkDestroyDescriptorPool(this->_device->logicalDevice, this->_descriptorPool, nullptr);
+    }
+
+    void DescriptorSets::_createDescriptorSetPool() {
         VkDescriptorPoolSize poolSize{};
         poolSize.type = this->_descriptorType;
-        poolSize.descriptorCount = this->_frameCount;
+        poolSize.descriptorCount = this->_descriptorCount;
         VkDescriptorPoolCreateInfo poolInfo{};
         poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
         poolInfo.poolSizeCount = 1;
         poolInfo.pPoolSizes = &poolSize;
-        poolInfo.maxSets = this->_frameCount;
+        poolInfo.maxSets = this->_descriptorCount;
         if (vkCreateDescriptorPool(this->_device->logicalDevice, &poolInfo, nullptr, &this->_descriptorPool) != VK_SUCCESS) {
             throw std::runtime_error("failed to create descriptor pool!");
         }
     }
 
-    void Descriptor::_createDescriptorLayout() {
+    void DescriptorSets::_createDescriptorSetLayout() {
         VkDescriptorSetLayoutBinding    uboBinding{};
         uboBinding.binding = this->_binding;
-        uboBinding.descriptorCount = 1;
+        uboBinding.descriptorCount = this->_descriptorCountPerSet;
         uboBinding.descriptorType = this->_descriptorType;
         uboBinding.stageFlags = this->_shaderStage;
         uboBinding.pImmutableSamplers = nullptr;
@@ -81,35 +94,85 @@ namespace netero::graphics {
         }
     }
 
-    void Descriptor::_createDescriptorSets() {
-        std::vector<VkDescriptorSetLayout> layouts(this->_frameCount, this->_descriptorSetLayout);
+    void DescriptorSets::_createDescriptorSets() {
+        std::vector<VkDescriptorSetLayout> layouts(this->_descriptorCount, this->_descriptorSetLayout);
         VkDescriptorSetAllocateInfo allocInfo{};
         allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
         allocInfo.descriptorPool = this->_descriptorPool;
-        allocInfo.descriptorSetCount = this->_frameCount;
+        allocInfo.descriptorSetCount = this->_descriptorCount;
         allocInfo.pSetLayouts = layouts.data();
-        this->_descriptorSets.resize(this->_frameCount);
+        this->_descriptorSets.resize(this->_descriptorCount);
         if (vkAllocateDescriptorSets(this->_device->logicalDevice, &allocInfo, this->_descriptorSets.data()) != VK_SUCCESS) {
             throw std::runtime_error("failed to allocate descriptor sets!");
         }
-        for (size_t i = 0; i < this->_frameCount; i++) {
-            VkDescriptorImageInfo   imageInfo{};
-            imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-            imageInfo.imageView = this->_imageView;
-            imageInfo.sampler = this->_textureSampler;
+    }
+
+    DescriptorSets::Error    DescriptorSets::write(int setIdx, VkImageView imageView, VkSampler sampler) {
+        if (this->_descriptorSets.empty()) {
+            return Error::EMPTY_DESCRIPTOR_SET;
+        }
+        if (this->_descriptorType != VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER) {
+            return Error::BAD_DESCRIPTOR_TYPE;
+        }
+        if (setIdx != WHOLE_SIZE && setIdx < 0 && setIdx >= this->_descriptorSets.size()) {
+            return Error::INDEX_OUT_OF_BOUND;
+        }
+        VkDescriptorImageInfo   imageInfo;
+        imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        imageInfo.imageView = imageView;
+        imageInfo.sampler = sampler;
+        this->_write(setIdx, nullptr, &imageInfo);
+        return Error::SUCCESS;
+    }
+
+    DescriptorSets::Error    DescriptorSets::write(int setIdx, VkBuffer buffer, VkDeviceSize offset, VkDeviceSize range) {
+        if (this->_descriptorSets.empty()) {
+            return Error::EMPTY_DESCRIPTOR_SET;
+        }
+        if (this->_descriptorType != VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER) {
+            return Error::BAD_DESCRIPTOR_TYPE;
+        }
+        if (setIdx != WHOLE_SIZE && setIdx < 0 && setIdx >= this->_descriptorSets.size()) {
+            return Error::INDEX_OUT_OF_BOUND;
+        }
+        VkDescriptorBufferInfo bufferInfo;
+        bufferInfo.buffer = buffer;
+        bufferInfo.offset = offset;
+        bufferInfo.range = range;
+        this->_write(setIdx, &bufferInfo, nullptr);
+        return Error::SUCCESS;
+    }
+
+    DescriptorSets::Error    DescriptorSets::_write(int setIdx, VkDescriptorBufferInfo* bufferInfo, VkDescriptorImageInfo* imageInfo) {
+        if (setIdx == WHOLE_SIZE) {
+            for (size_t i = 0; i < this->_descriptorCount; i++) {
+                VkWriteDescriptorSet descriptorWrite{};
+                descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+                descriptorWrite.dstSet = this->_descriptorSets[i];
+                descriptorWrite.dstBinding = this->_binding;
+                descriptorWrite.dstArrayElement = 0;
+                descriptorWrite.descriptorType = this->_descriptorType;
+                descriptorWrite.descriptorCount = this->_descriptorCountPerSet;
+                descriptorWrite.pBufferInfo = bufferInfo ? bufferInfo : nullptr;
+                descriptorWrite.pImageInfo = imageInfo ? imageInfo : nullptr;
+                descriptorWrite.pTexelBufferView = nullptr;
+                vkUpdateDescriptorSets(this->_device->logicalDevice, 1, &descriptorWrite, 0, nullptr);
+            }
+        }
+        else {
             VkWriteDescriptorSet descriptorWrite{};
             descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-            descriptorWrite.dstSet = this->_descriptorSets[i];
-            descriptorWrite.dstBinding = 1;
+            descriptorWrite.dstSet = this->_descriptorSets[setIdx];
+            descriptorWrite.dstBinding = this->_binding;
             descriptorWrite.dstArrayElement = 0;
-            descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-            descriptorWrite.descriptorCount = 1;
-            descriptorWrite.pBufferInfo = nullptr;
-            descriptorWrite.pImageInfo = &imageInfo;
+            descriptorWrite.descriptorType = this->_descriptorType;
+            descriptorWrite.descriptorCount = this->_descriptorCountPerSet;
+            descriptorWrite.pBufferInfo = bufferInfo ? bufferInfo : nullptr;
+            descriptorWrite.pImageInfo = imageInfo ? imageInfo : nullptr;
             descriptorWrite.pTexelBufferView = nullptr;
             vkUpdateDescriptorSets(this->_device->logicalDevice, 1, &descriptorWrite, 0, nullptr);
         }
+        return Error::SUCCESS;
     }
-
 }
 
