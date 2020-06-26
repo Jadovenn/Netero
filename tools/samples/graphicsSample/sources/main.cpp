c716d2194b2d4a8756f0b93d3df211a10b6e0985
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-06-26 17:43:14 +0200

    tools/samples/ formating on graphicsSample

--- tools/samples/graphicsSample/sources/main.cpp
+++ tools/samples/graphicsSample/sources/main.cpp
@@ -3,52 +3,57 @@
  * see LICENSE.txt
  */
 
-#include <iostream>
 #include <chrono>
-#include <netero/os.hpp>
+#include <iostream>
+
 #include <netero/graphics/application.hpp>
 #include <netero/graphics/context.hpp>
 #include <netero/graphics/vertex.hpp>
+#include <netero/os.hpp>
 
-const std::string g_ModelVertexShader_path = netero::os::getBundlePath() + "/shaders/ModelVertex.spv";
-const std::string g_ColorFragmentShader_path = netero::os::getBundlePath() + "/shaders/ColorFragment.spv";
-const std::string g_TexturedModelVertexShader_path = netero::os::getBundlePath() + "/shaders/TexturedModelVertex.spv";
-const std::string g_TexturedColorFragmentShader_path = netero::os::getBundlePath() + "/shaders/TexturedColorFragment.spv";
+const std::string g_ModelVertexShader_path =
+    netero::os::getBundlePath() + "/shaders/ModelVertex.spv";
+const std::string g_ColorFragmentShader_path =
+    netero::os::getBundlePath() + "/shaders/ColorFragment.spv";
+const std::string g_TexturedModelVertexShader_path =
+    netero::os::getBundlePath() + "/shaders/TexturedModelVertex.spv";
+const std::string g_TexturedColorFragmentShader_path =
+    netero::os::getBundlePath() + "/shaders/TexturedColorFragment.spv";
 const std::string g_texture = netero::os::getBundlePath() + "/textures/cat.png";
 
-std::vector<netero::graphics::Vertex>   squareVertices {
-    {{-0.5f, -0.5f}, {1.f, 0.0f, 0.f}, {0.f, 0.f}},
-    {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {1.f, 0.f}},
-    {{0.5f, 0.5f}, {0.f, 0.0f, 1.f}, {1.f, 1.f}},
-    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}, {0.f, 1.f}}
+std::vector<netero::graphics::Vertex> squareVertices {
+    { { -0.5f, -0.5f }, { 1.f, 0.0f, 0.f }, { 0.f, 0.f } },
+    { { 0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }, { 1.f, 0.f } },
+    { { 0.5f, 0.5f }, { 0.f, 0.0f, 1.f }, { 1.f, 1.f } },
+    { { -0.5f, 0.5f }, { 1.0f, 1.0f, 1.0f }, { 0.f, 1.f } }
 };
 
-std::vector<uint16_t>    squareIndices{
-    0, 1, 2, 2, 3, 0
-};
+std::vector<uint16_t> squareIndices { 0, 1, 2, 2, 3, 0 };
 
-std::vector<netero::graphics::Vertex>   triangleVertices {
-    {{0.f, -0.2f}, {1.f, 0.0f, 0.f}},
-    {{0.25f, 0.25f}, {0.0f, 1.0f, 0.0f}},
-    {{-0.25f, 0.25f}, {0.f, 0.0f, 1.f}},
+std::vector<netero::graphics::Vertex> triangleVertices {
+    { { 0.f, -0.2f }, { 1.f, 0.0f, 0.f } },
+    { { 0.25f, 0.25f }, { 0.0f, 1.0f, 0.0f } },
+    { { -0.25f, 0.25f }, { 0.f, 0.0f, 1.f } },
 };
 
 struct GraphicSampleApp {
-    netero::graphics::Application*  handle = nullptr;
-    netero::graphics::Context*      context = nullptr;
-    netero::graphics::Model*        square = nullptr;
-    netero::graphics::Model*        triangle = nullptr;
-    netero::graphics::Instance*     squareInstance1 = nullptr;
-    netero::graphics::Instance*     squareInstance2 = nullptr;
-    netero::graphics::Instance*     triangleInstance1 = nullptr;
+    netero::graphics::Application* handle = nullptr;
+    netero::graphics::Context*     context = nullptr;
+    netero::graphics::Model*       square = nullptr;
+    netero::graphics::Model*       triangle = nullptr;
+    netero::graphics::Instance*    squareInstance1 = nullptr;
+    netero::graphics::Instance*    squareInstance2 = nullptr;
+    netero::graphics::Instance*    triangleInstance1 = nullptr;
 };
 
-void    RunApplication(GraphicSampleApp* myApp) {
+void RunApplication(GraphicSampleApp* myApp)
+{
     const auto startTime = std::chrono::high_resolution_clock::now();
     myApp->context->build();
     while (!myApp->context->shouldClose()) {
-        auto now = std::chrono::high_resolution_clock::now();
-        const float time = std::chrono::duration<float, std::chrono::seconds::period>(now - startTime).count();
+        auto        now = std::chrono::high_resolution_clock::now();
+        const float time =
+            std::chrono::duration<float, std::chrono::seconds::period>(now - startTime).count();
         myApp->triangleInstance1->z.rotate(time * glm::radians(45.0f));
         myApp->squareInstance1->z.rotate(time * glm::radians(90.0f));
         myApp->squareInstance2->z.rotate(-1 * time * glm::radians(30.0f));
@@ -57,17 +62,22 @@ void    RunApplication(GraphicSampleApp* myApp) {
     myApp->context->release();
 }
 
-void    InitializeApp(GraphicSampleApp* myApp) {
+void InitializeApp(GraphicSampleApp* myApp)
+{
     myApp->handle = netero::graphics::Application::Initialize("GraphicsSample");
-    myApp->context = myApp->handle->newWindowedContext(800, 600, netero::graphics::WindowMode::RESIZABLE);
+    myApp->context =
+        myApp->handle->newWindowedContext(800, 600, netero::graphics::WindowMode::RESIZABLE);
     myApp->square = myApp->context->createModel();
-    myApp->square->loadShader(g_TexturedModelVertexShader_path, netero::graphics::ShaderStage::VERTEX);
-    myApp->square->loadShader(g_TexturedColorFragmentShader_path, netero::graphics::ShaderStage::FRAGMENT);
+    myApp->square->loadShader(g_TexturedModelVertexShader_path,
+                              netero::graphics::ShaderStage::VERTEX);
+    myApp->square->loadShader(g_TexturedColorFragmentShader_path,
+                              netero::graphics::ShaderStage::FRAGMENT);
     myApp->square->addVertices(squareVertices, squareIndices);
     myApp->square->loadTexture(g_texture, netero::graphics::TextureSamplingMode::REPEAT);
     myApp->triangle = myApp->context->createModel();
     myApp->triangle->loadShader(g_ModelVertexShader_path, netero::graphics::ShaderStage::VERTEX);
-    myApp->triangle->loadShader(g_ColorFragmentShader_path, netero::graphics::ShaderStage::FRAGMENT);
+    myApp->triangle->loadShader(g_ColorFragmentShader_path,
+                                netero::graphics::ShaderStage::FRAGMENT);
     myApp->triangle->addVertices(triangleVertices);
     myApp->squareInstance1 = myApp->square->createInstance();
     myApp->squareInstance1->x = 0.5;
@@ -85,7 +95,8 @@ void    InitializeApp(GraphicSampleApp* myApp) {
     myApp->triangleInstance1->y.scale(2);
 }
 
-void    CleanUpApp(GraphicSampleApp* myApp) {
+void CleanUpApp(GraphicSampleApp* myApp)
+{
     myApp->square->deleteInstance(myApp->squareInstance1);
     myApp->square->deleteInstance(myApp->squareInstance2);
     myApp->triangle->deleteInstance(myApp->triangleInstance1);
@@ -95,27 +106,28 @@ void    CleanUpApp(GraphicSampleApp* myApp) {
 }
 
 #if defined(WIN_APP)
-#include <windows.h>
 #include <WinBase.h>
-int WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#include <windows.h>
+int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
 #else
-int    main() {
+int main()
+{
 #endif
-    int rtCode = 0;
-    GraphicSampleApp    myApp{};
+    int              rtCode = 0;
+    GraphicSampleApp myApp {};
     try {
         InitializeApp(&myApp);
         RunApplication(&myApp);
         CleanUpApp(&myApp);
     }
-    catch (const std::exception &e) {
+    catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
         rtCode = 1;
     }
-    catch(...) {
+    catch (...) {
         std::cerr << "Fatal error." << std::endl;
         rtCode = 2;
     }
     return rtCode;
 }
-
