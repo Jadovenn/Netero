5276cef7a5d95161e734da94f246d06698b5a258
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-06-29 23:21:51 +0200

    graphics/vertex/ perform clean & formating

--- include/netero/graphics/vertex.hpp
+++ include/netero/graphics/vertex.hpp
@@ -7,67 +7,71 @@
 
 #include <array>
 #include <vector>
-#include <vulkan/vulkan.h>
+
 #include <glm/glm.hpp>
+#include <vulkan/vulkan.h>
+
 #include <netero/graphics/device.hpp>
 
 namespace netero::graphics {
 
-    struct Vertex {
-        glm::vec2   pos;
-        glm::vec3   color;
-        glm::vec2   texCord = {0, 0};
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+    glm::vec2 texCord = { 0, 0 };
 
-        static VkVertexInputBindingDescription  getBindingDescription() {
-            VkVertexInputBindingDescription bindingDescription = {};
+    static VkVertexInputBindingDescription getBindingDescription()
+    {
+        VkVertexInputBindingDescription bindingDescription = {};
 
-            bindingDescription.binding = 0;
-            bindingDescription.stride = sizeof(Vertex);
-            bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-            return bindingDescription;
-        }
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(Vertex);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return bindingDescription;
+    }
 
-        static std::array<VkVertexInputAttributeDescription, 3>    getAttributeDescription() {
-            std::array<VkVertexInputAttributeDescription, 3>    attributeDescriptions{};
-            attributeDescriptions[0].binding = 0;
-            attributeDescriptions[0].location = 0;
-            attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-            attributeDescriptions[0].offset = offsetof(Vertex, pos);
-            attributeDescriptions[1].binding = 0;
-            attributeDescriptions[1].location = 1;
-            attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-            attributeDescriptions[1].offset = offsetof(Vertex, color);
-            attributeDescriptions[2].binding = 0;
-            attributeDescriptions[2].location = 2;
-            attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-            attributeDescriptions[2].offset = offsetof(Vertex, texCord);
+    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescription()
+    {
+        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions {};
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(Vertex, texCord);
 
-            return attributeDescriptions;
-        }
-    };
+        return attributeDescriptions;
+    }
+};
 
-    struct VertexBuffer {
-        Device* _device;
-        void    createVertexBuffer();
-        void    createIndexBuffer(int);
+class VertexBuffer {
+    Device* _device;
+    void    createVertexBuffer();
+    void    createIndexBuffer(int);
+
     public:
-        VertexBuffer(Device*);
-        VertexBuffer(const VertexBuffer&) = delete;
-        VertexBuffer(VertexBuffer&&) = delete;
-        VertexBuffer& operator=(const VertexBuffer&) = delete;
-        VertexBuffer& operator=(VertexBuffer&&) = delete;
-        ~VertexBuffer();
+    VertexBuffer(Device*);
+    VertexBuffer(const VertexBuffer&) = delete;
+    VertexBuffer(VertexBuffer&&) = delete;
+    VertexBuffer& operator=(const VertexBuffer&) = delete;
+    VertexBuffer& operator=(VertexBuffer&&) = delete;
+    ~VertexBuffer();
 
-        void    AllocateAndTransfer(int);
-        void    release();
+    void AllocateAndTransfer(int);
+    void release();
 
-        VkBuffer            vertexBuffer;
-        VkDeviceMemory      vertexBufferMemory;
-        std::vector<Vertex> vertices;
-        VkBuffer                indexBuffer;
-        VkDeviceMemory          indexBufferMemory;
-        std::vector<uint16_t>   indices;
-    };
+    VkBuffer              vertexBuffer;
+    VkDeviceMemory        vertexBufferMemory;
+    std::vector<Vertex>   vertices;
+    VkBuffer              indexBuffer;
+    VkDeviceMemory        indexBufferMemory;
+    std::vector<uint16_t> indices;
+};
 
-}
-
+} // namespace netero::graphics
