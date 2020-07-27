34c4ee6f7980b204de08d274e0999a73b76d50c2
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-07-27 20:04:14 +0200

    cmake/ add nasm integration script

---
+++ cmake/enable_nasm.cmake
@@ -0,0 +1,32 @@
+
+if(WIN32)
+
+    set(NASM_PATH "${CMAKE_CURRENT_SOURCE_DIR}/bin/nasm/win64/nasm.exe")
+    set(NASM_FLAGS "-f win64 -dWIN64=1")
+
+elseif(APPLE AND NOT UNIX)
+
+    set(NASM_PATH "${CMAKE_CURRENT_SOURCE_DIR}/bin/nasm/mach-o/nasm")
+    set(NASM_FLAGS "-f macho64 -dMACHO64=1")
+
+else()
+
+    set(NASM_PATH "/usr/bin/nasm")
+    set(NASM_FLAGS "-f elf64 -dELF64=1")
+
+endif(WIN32)
+
+if(NOT EXISTS ${NASM_PATH})
+
+    message(" NASM_PATH: ${NASM_PATH}")
+    message(FATAL_ERROR " - Cound not find NASM, make sure nasm is installed or NASM_PATH variable is correctly set.")
+
+endif(NOT EXISTS ${NASM_PATH})
+
+set(CMAKE_ASM_NASM_COMPILER ${NASM_PATH} CACHE INTERNAL "" FORCE)
+enable_language(ASM_NASM)
+
+set(CMAKE_ASM_NASM_COMPILER_ARG1 "" CACHE INTERNAL "" FORCE)
+set(CMAKE_ASM_NASM_FLAGS ${NASM_FLAGS} CACHE INTERNAL "" FORCE)
+set(CMAKE_ASM_NASM_SOURCE_FILE_EXTENSIONS "asm;nasm;s;S")
+
