28fbc9db8950145a4eefc16ad591ecba281a09dd
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-07-29 23:53:14 +0200

    audio/ add NativeCallbackHandler and format fetch in DeviceImpl for coreAudio

--- modules/audio/private/backends/coreAudio/deviceImpl.cpp
+++ modules/audio/private/backends/coreAudio/deviceImpl.cpp
@@ -5,8 +5,6 @@
 
 #include "deviceImpl.hpp"
 
-#include <cstring>
-
 netero::audio::Device::~Device() = default;
 
 static std::string cf_string_to_std_string(CFStringRef &cf_str)
@@ -26,13 +24,14 @@ static std::string cf_string_to_std_string(CFStringRef &cf_str)
     return str;
 }
 
-DeviceImpl::DeviceImpl(UInt32 id): _id(id), _isValid(true)
+DeviceImpl::DeviceImpl(UInt32 id, AudioObjectPropertyScope scope)
+    : _id(id), _isValid(true), _scope(scope)
 {
     assert(_id);
 
     UInt32                     size = sizeof(CFStringRef);
     AudioObjectPropertyAddress property = { kAudioObjectPropertyName,
-                                            kAudioObjectPropertyScopeGlobal,
+                                            this->_scope,
                                             kAudioObjectPropertyElementMaster };
     CFStringRef                cf_str;
     OSStatus result = AudioObjectGetPropertyData(id, &property, 0, nullptr, &size, &cf_str);
@@ -51,6 +50,139 @@ DeviceImpl::DeviceImpl(UInt32 id): _id(id), _isValid(true)
     }
     this->_manufacturer = cf_string_to_std_string(cf_str);
     CFRelease(cf_str);
+
+    AudioBufferList *bufferList = nullptr;
+    property.mSelector = kAudioDevicePropertyStreamConfiguration;
+    size = 0;
+    result = AudioObjectGetPropertyDataSize(id, &property, 0, nullptr, &size);
+    if (result != noErr || size == 0) {
+        this->_isValid = false;
+        return;
+    }
+    bufferList = new (std::nothrow) AudioBufferList[size];
+    if (!bufferList) {
+        this->_isValid = false;
+        return;
+    }
+    result = AudioObjectGetPropertyData(id, &property, 0, nullptr, &size, bufferList);
+    if (result != noErr || size == 0) {
+        delete[] bufferList;
+        this->_isValid = false;
+        return;
+    }
+    unsigned nStreams = bufferList->mNumberBuffers;
+    for (unsigned idx = 0; idx < nStreams; idx++) {
+        this->_format.channels = bufferList->mBuffers[idx].mNumberChannels;
+        this->_format.framesCount = bufferList->mBuffers[idx].mDataByteSize;
+    }
+    this->_format.bytesPerSample = sizeof(float);
+    this->_format.bytesPerFrame = sizeof(float) * this->_format.channels;
+    delete[] bufferList;
+
+    //======================================
+    // Retrieve supported sampling rate
+    // - Retrieve the supported min and max rate,
+    //   then translate to a uniform list of
+    //   supported rate, then choose 48000Hz rate
+    //   or the higher rate by default
+    //======================================
+    property.mSelector = kAudioDevicePropertyAvailableNominalSampleRates;
+    result = AudioObjectGetPropertyDataSize(id, &property, 0, nullptr, &size);
+    if (result != kAudioHardwareNoError || size == 0) {
+        this->_isValid = false;
+    }
+    UInt32          nRanges = size / sizeof(AudioValueRange);
+    AudioValueRange rangeList[nRanges];
+    result = AudioObjectGetPropertyData(id, &property, 0, nullptr, &size, &rangeList);
+    if (result != kAudioHardwareNoError) {
+        this->_isValid = false;
+    }
+    Float64            minimumRate = 1.0, maximumRate = 10000000000.0;
+    bool               haveValueRange = false;
+    std::vector<float> sampleRates;
+    for (unsigned idx = 0; idx < nRanges; idx++) {
+        if (rangeList[idx].mMinimum == rangeList[idx].mMaximum) {
+            auto samplingRate = (float)rangeList[idx].mMinimum;
+            this->_format.supportedSamplingRate.push_back(samplingRate);
+            this->_format.samplingFrequency = samplingRate;
+        }
+        else {
+            haveValueRange = true;
+            if (rangeList[idx].mMinimum > minimumRate) {
+                minimumRate = rangeList[idx].mMinimum;
+            }
+            if (rangeList[idx].mMaximum < maximumRate) {
+                maximumRate = rangeList[idx].mMaximum;
+            }
+        }
+    }
+    if (haveValueRange) {
+        for (auto samplingRate : SamplingRates) {
+            if (samplingRate >= (unsigned)minimumRate && samplingRate <= (unsigned)maximumRate) {
+                this->_format.supportedSamplingRate.push_back(samplingRate);
+            }
+        }
+    }
+    Float64 nominalRate;
+    size = sizeof(Float64);
+    property.mSelector = kAudioDevicePropertyNominalSampleRate;
+    result = AudioObjectGetPropertyData(id, &property, 0, nullptr, &size, &nominalRate);
+    if (result != noErr) {
+        this->_isValid = false;
+        return;
+    }
+    this->_format.samplingFrequency = nominalRate;
+
+    std::sort(this->_format.supportedSamplingRate.begin(),
+              this->_format.supportedSamplingRate.end());
+    this->_format.supportedSamplingRate.erase(
+        std::unique(this->_format.supportedSamplingRate.begin(),
+                    this->_format.supportedSamplingRate.end()),
+        this->_format.supportedSamplingRate.end());
+    if (this->_format.supportedSamplingRate.empty()) {
+        this->_isValid = false;
+    }
+    this->_format.supportedSamplingRate.shrink_to_fit();
 }
 
 DeviceImpl::~DeviceImpl() noexcept = default;
+
+OSStatus DeviceImpl::NativeCallbackHandler(AudioDeviceID inDevice,
+                                           const AudioTimeStamp *,
+                                           const AudioBufferList *inInputData,
+                                           const AudioTimeStamp *,
+                                           AudioBufferList *outOutputData,
+                                           const AudioTimeStamp *,
+                                           void *context)
+{
+    auto *device = reinterpret_cast<DeviceImpl *>(context);
+    if (device->_id == inDevice) {
+        if (device->_scope == kAudioObjectPropertyScopeOutput) {
+            // handle output here
+        }
+        else if (device->_scope == kAudioObjectPropertyScopeInput) {
+            // handle input here
+        }
+        else {
+            return 1;
+        }
+    }
+    return kAudioHardwareNoError;
+}
+
+netero::audio::Device::RtCode DeviceImpl::open()
+{
+    if (this->_scope == kAudioObjectPropertyScopeOutput && !this->_processingCallback) {
+        return RtCode::NO_REGISTERED_CALLBACK;
+    }
+    if (this->_scope == kAudioObjectPropertyScopeInput && !this->_acquisitionCallback) {
+        return RtCode::NO_REGISTERED_CALLBACK;
+    }
+
+    return RtCode::SUCCESS;
+}
+
+netero::audio::Device::RtCode DeviceImpl::close()
+{
+    return RtCode::SUCCESS;
+}
--- modules/audio/private/backends/coreAudio/deviceImpl.hpp
+++ modules/audio/private/backends/coreAudio/deviceImpl.hpp
@@ -9,20 +9,73 @@
 
 #include <CoreAudio/CoreAudio.h>
 
+static constexpr unsigned SamplingRates[] = { 4000,  5512,  8000,  9600,  11025, 16000,  22050,
+                                              32000, 44100, 48000, 88200, 96000, 176400, 192000 };
+
 class DeviceImpl final: public netero::audio::Device {
     public:
-    explicit DeviceImpl(UInt32 id);
+    explicit DeviceImpl(UInt32 id, AudioObjectPropertyScope scope);
     ~DeviceImpl() noexcept final;
 
-    RtCode                           open() final { return RtCode::SUCCESS; }
-    RtCode                           close() final { return RtCode::SUCCESS; }
+    bool operator==(UInt32 deviceUID) const noexcept { return this->_id == deviceUID; }
+
+    RtCode open() final;
+    RtCode close() final;
+
     [[nodiscard]] bool               isValid() const final { return this->_isValid; }
+    [[nodiscard]] bool               isLoopback() const final { return false; }
     [[nodiscard]] const std::string& getName() const final { return this->_name; }
     [[nodiscard]] const std::string& getManufacturer() const final { return this->_manufacturer; };
 
+    [[nodiscard]] const netero::audio::Format& getFormat() const final { return this->_format; }
+
+    void setProcessingCallback(ProcessingCallbackHandle& cb) final {
+        this->_processingCallback = cb;
+    }
+
+    void resetProcessingCallback() final {
+        this->_processingCallback = nullptr;
+    }
+
+    void setAcquisitionCallback(AcquisitionCallbackHandle& cb) final {
+        this->_acquisitionCallback = cb;
+    }
+
+    void resetAcquisitionCallback() final {
+        this->_acquisitionCallback = nullptr;
+    }
+
+    void setErrorCallback(ErrorCallbackHandle& cb) final {
+        this->_errorCallback = cb;
+    }
+
+    void resetErrorCallback() final {
+        this->_errorCallback = nullptr;
+    }
+
+    static OSStatus NativeCallbackHandler(AudioDeviceID inDevice,
+                                          const AudioTimeStamp*,
+                                          const AudioBufferList* inInputData,
+                                          const AudioTimeStamp*,
+                                          AudioBufferList* outOutputData,
+                                          const AudioTimeStamp*,
+                                          void* context);
+
+    void reportError(const std::string& anErrorMessage) {
+        if (this->_errorCallback) {
+            this->_errorCallback(anErrorMessage);
+        }
+    }
+
     private:
-    bool   _isValid;
-    UInt32 _id;
+    bool                     _isValid;
+    UInt32                   _id;
+    AudioObjectPropertyScope _scope;
+
+    netero::audio::Format     _format;
+    ProcessingCallbackHandle  _processingCallback;
+    AcquisitionCallbackHandle _acquisitionCallback;
+    ErrorCallbackHandle       _errorCallback;
 
     std::string _name;
     std::string _manufacturer;
