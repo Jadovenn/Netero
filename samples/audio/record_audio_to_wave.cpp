24dae167d8ec74153ee715c5beb497c8267396ea
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-08-05 23:00:31 +0200

    samples/audio/ update audio record sample to record in a wave file

--- samples/audio/record_audio_to_wave.cpp
+++ samples/audio/record_audio_to_wave.cpp
@@ -3,36 +3,71 @@
  * see LICENSE.txt
  */
 
+#include <iostream>
 #include <thread>
 
 #include <netero/audio/deviceManager.hpp>
-#include <netero/netero.hpp>
+#include <netero/audio/waveFile.hpp>
+#include <netero/logger.hpp>
+
+int ChooseDevice()
+{
+    auto& deviceManager = netero::audio::DeviceManager::GetInstance();
+    deviceManager.scanForDevices();
+    auto& devices = deviceManager.getInputDevices();
+
+    int defaultInputDeviceIdx = deviceManager.getDefaultInputDeviceIdx();
+    LOG << "Choose a Device:" << std::endl;
+    if (defaultInputDeviceIdx >= 0) {
+        LOG << "Default input device: " << devices[defaultInputDeviceIdx]->getName() << std::endl;
+    }
+    int idx = 0;
+    for (auto& device : devices) {
+        LOG << idx << ": " << device->getName() << std::endl;
+    }
+
+    LOG << "idx ? => ";
+    std::cin >> idx;
+    return idx;
+}
 
 int main()
 {
     auto& deviceManager = netero::audio::DeviceManager::GetInstance();
-    if (!deviceManager.isValid()) {
+
+    int idx = ChooseDevice();
+    deviceManager.scanForDevices();
+    auto& devices = deviceManager.getInputDevices();
+
+    if (idx < 0 || idx >= devices.size()) {
+        LOG << "Idx out of range you try to trick me or devices have changed" << std::endl;
         return 1;
     }
 
-    deviceManager.scanForDevices();
-    auto& devices = deviceManager.getInputDevices();
-    int   defaultInputDeviceIdx = deviceManager.getDefaultInputDeviceIdx();
-    if (defaultInputDeviceIdx < 0) {
-        return 1; // Could not fetch default device properly
+    auto&                   device = devices[idx];
+    netero::audio::WaveFile saveFile;
+
+    const auto& format = device->getFormat();
+    saveFile.setFormat(format);
+    std::string fileName;
+    fileName += std::to_string(format.channels);
+    fileName += "channels_";
+    fileName += std::to_string(format.samplingFrequency);
+    fileName += "Hz.wav";
+    saveFile.open(fileName, std::fstream::out | std::fstream::trunc | std::fstream::binary);
+
+    if (!saveFile.is_open()) {
+        LOG << "Failed to create " << fileName << "file." << std::endl;
+        return 1;
     }
-    auto* device = devices[0];
 
-    auto                                        format = device->getFormat();
-    std::function<void(const float*, unsigned)> acquisitionCallback = [](const float* buffer,
-                                                                         unsigned     frames) {
-        (void)buffer;
-        (void)frames;
-    };
+    std::function<void(const float*, unsigned)> acquisitionCallback =
+        [&saveFile, format](const float* buffer, unsigned frames) {
+            saveFile.write(buffer, frames * format.channels);
+        };
+
     device->setAcquisitionCallback(acquisitionCallback);
-
     device->open();
-
     auto start = std::chrono::system_clock::now();
     while (10 > std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() -
                                                                  start)
@@ -41,6 +76,7 @@ int main()
     }
 
     device->close();
+    saveFile.close();
 
     return 0;
 }
