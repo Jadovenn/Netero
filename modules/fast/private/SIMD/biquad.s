6747d061aa49a84526f8a74eaac072d2420e57cb
Jean-Adrien Domage <jean-adrien.domage@ubisoft.com>
2020-08-06 23:03:07 +0200

    fast/ remove fp_true_peak, interpolation_filter, low_pass_filter

--- modules/fast/private/SIMD/biquad.s
+++ modules/fast/private/SIMD/biquad.s
@@ -1,10 +1,6 @@
 section .text
 bits 64
 
-%include ./common.s
-
 global biquad
 biquad:
-	alloc_framestack
-	free_framestack
 	ret
--- modules/fast/private/SIMD/true_pick/fp_true_pick.s
+++
@@ -1,7 +0,0 @@
-section .text
-bits 64
-
-global fp_true_pick
-fp_true_pick:
-	ret
-
