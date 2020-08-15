1ed225b837188c325519bb2867fe8a979555e989
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-08-15 17:38:03 +0200

    graphics/ add utils/vkImage.cpp

---
+++ modules/graphics/private/utils/vkImage.cpp
@@ -0,0 +1,64 @@
+/**
+ * Netero sources under BSD-3-Clause
+ * see LICENSE.txt
+ */
+
+#include <netero/graphics/device.hpp>
+
+#include "vkUtils.hpp"
+
+namespace vkUtils {
+
+VkImageView CreateImageView(VkDevice device, VkImage image, VkFormat format)
+{
+    VkImageView           imageView;
+    VkImageViewCreateInfo viewInfo {};
+    viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    viewInfo.image = image;
+    viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+    viewInfo.format = format;
+    viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    viewInfo.subresourceRange.baseMipLevel = 0;
+    viewInfo.subresourceRange.levelCount = 1;
+    viewInfo.subresourceRange.baseArrayLayer = 0;
+    viewInfo.subresourceRange.layerCount = 1;
+    if (vkCreateImageView(device, &viewInfo, nullptr, &imageView) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create texture image view!");
+    }
+    return imageView;
+}
+
+VkFormat SelectSupportedImageFormat(netero::graphics::Device&    device,
+                                    const std::vector<VkFormat>& candidates,
+                                    VkImageTiling                tiling,
+                                    VkFormatFeatureFlags         features)
+{
+    for (VkFormat format : candidates) {
+        VkFormatProperties props;
+        vkGetPhysicalDeviceFormatProperties(device.physicalDevice, format, &props);
+        if (tiling == VK_IMAGE_TILING_LINEAR &&
+            (props.linearTilingFeatures & features) == features) {
+            return format;
+        }
+        else if (tiling == VK_IMAGE_TILING_OPTIMAL &&
+                 (props.optimalTilingFeatures & features) == features) {
+            return format;
+        }
+    }
+    return VK_FORMAT_UNDEFINED;
+}
+
+VkFormat FindDepthBufferingImageFormat(netero::graphics::Device& device)
+{
+    return SelectSupportedImageFormat(
+        device,
+        { VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
+        VK_IMAGE_TILING_OPTIMAL,
+        VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT);
+}
+
+bool HasStencilComponent(VkFormat format) {
+    return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT;
+}
+
+} // namespace vkUtils
