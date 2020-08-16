5c54f2e9451e70b3c34c968fe452e7ad495021fb
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-08-16 18:17:21 +0200

    sample/ update NeteroModelGraphics to use indices while loading obj model

--- samples/graphicsModel/main.cpp
+++ samples/graphicsModel/main.cpp
@@ -5,6 +5,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <unordered_map>
 
 #include <netero/extra/tiny_obj_loader.hpp>
 #include <netero/graphics/application.hpp>
@@ -55,32 +56,40 @@ void InitializeApp(GraphicSampleApp* myApp)
                                    netero::graphics::ShaderStage::FRAGMENT);
     myApp->vikingHouse->loadTexture(VikingHouseTexturePath,
                                     netero::graphics::TextureSamplingMode::REPEAT);
-    tinyobj::attrib_t                     attrib;
-    std::vector<netero::graphics::Vertex> vertices;
-    std::vector<tinyobj::shape_t>         shapes;
-    std::vector<tinyobj::material_t>      materials;
-    std::string                           warn, err;
-    if (!tinyobj::LoadObj(&attrib,
-                          &shapes,
-                          &materials,
-                          &warn,
-                          &err,
-                          VikingHouseModelPath.c_str())) {
-        throw std::runtime_error(warn + err);
-    }
-    for (const auto& shape : shapes) {
-        for (const auto& index : shape.mesh.indices) {
-            netero::graphics::Vertex vertex {};
-            vertex.pos = { attrib.vertices[3 * index.vertex_index + 0],
-                           attrib.vertices[3 * index.vertex_index + 1],
-                           attrib.vertices[3 * index.vertex_index + 2] };
-            vertex.texCord = { attrib.texcoords[2 * index.texcoord_index + 0],
-                               1.0f - attrib.texcoords[2 * index.texcoord_index + 1] };
-            vertex.color = { 1.0f, 1.0f, 1.0f };
-            vertices.push_back(vertex);
+    {
+        tinyobj::attrib_t                     attrib;
+        std::vector<netero::graphics::Vertex> vertices;
+        std::vector<uint32_t>                 indices;
+        std::vector<tinyobj::shape_t>         shapes;
+        std::vector<tinyobj::material_t>      materials;
+        std::string                           warn, err;
+        if (!tinyobj::LoadObj(&attrib,
+                              &shapes,
+                              &materials,
+                              &warn,
+                              &err,
+                              VikingHouseModelPath.c_str())) {
+            throw std::runtime_error(warn + err);
         }
+        std::unordered_map<netero::graphics::Vertex, uint32_t> uniqueVertices {};
+        for (const auto& shape : shapes) {
+            for (const auto& index : shape.mesh.indices) {
+                netero::graphics::Vertex vertex {};
+                vertex.pos = { attrib.vertices[3 * index.vertex_index + 0],
+                               attrib.vertices[3 * index.vertex_index + 1],
+                               attrib.vertices[3 * index.vertex_index + 2] };
+                vertex.texCord = { attrib.texcoords[2 * index.texcoord_index + 0],
+                                   1.0f - attrib.texcoords[2 * index.texcoord_index + 1] };
+                vertex.color = { 1.0f, 1.0f, 1.0f };
+                if (uniqueVertices.count(vertex) == 0) {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices.at(vertex));
+            }
+        }
+        myApp->vikingHouse->addVertices(vertices, indices);
     }
-    myApp->vikingHouse->addVertices(vertices);
     myApp->vikingHouseInstance = myApp->vikingHouse->createInstance();
     myApp->vikingHouseInstance->y.rotate(glm::radians(20.f));
 }
