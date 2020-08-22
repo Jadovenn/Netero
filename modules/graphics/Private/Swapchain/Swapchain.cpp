f1ec3fb121c4cbb06274ff522783d59b9c9c5702
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-08-22 21:29:44 +0200

    graphics/ update Swapchain, now functional

--- modules/graphics/Private/Swapchain/Swapchain.cpp
+++ modules/graphics/Private/Swapchain/Swapchain.cpp
@@ -3,6 +3,7 @@
  * see LICENSE.txt
  */
 
+#include <array>
 #include <cassert>
 
 #include <Vulkan/Context/Context.hpp>
@@ -14,17 +15,18 @@ namespace Netero::Gfx {
 
 Swapchain::Swapchain(Context& aContext, uint32_t aMaxInFlightFrameCount)
     : myContext(aContext),
+      myRenderPass(aContext),
       myCurrentFrame(0),
       myFrameCount(0),
-      myMaximumInFligthFrames(aMaxInFlightFrameCount),
+      myMaximumInFlightFrames(aMaxInFlightFrameCount),
       mySwapchain(nullptr),
       mySwapchainExtent { 0, 0 },
-      mySwapchainImageFormat(VK_FORMAT_UNDEFINED)
+      mySwapchainImageFormat(VK_FORMAT_UNDEFINED),
+      myDepthImage(nullptr),
+      myDepthImageMemory(nullptr),
+      myDepthImageView(nullptr),
+      myCommandPool(nullptr)
 {
-    this->myAvailableFrameSemaphore.resize(myMaximumInFligthFrames);
-    this->myRenderedFrameSemaphore.resize(myMaximumInFligthFrames);
-    this->myInFlyFrameFence.resize(myMaximumInFligthFrames);
-    this->myRenderedFrameFence.resize(myMaximumInFligthFrames);
 }
 
 Swapchain::RtCode Swapchain::Initialize()
@@ -32,12 +34,17 @@ Swapchain::RtCode Swapchain::Initialize()
     assert(myContext.myLogicalDevice != nullptr);  // Logical must be ready at this point
     assert(myContext.myPhysicalDevice != nullptr); // Physical must be ready at this point
     assert(myContext.mySurface != nullptr);        // Surface must be ready at this point
+    CreateSwapchain();
+    this->myAvailableFrameSemaphore.resize(myMaximumInFlightFrames);
+    this->myRenderedFrameSemaphore.resize(myMaximumInFlightFrames);
+    this->myInFlyFence.resize(myMaximumInFlightFrames);
+    this->myInFlyFrameFence.resize(myFrameCount, nullptr);
     VkSemaphoreCreateInfo semaphoreInfo {};
     semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
     VkFenceCreateInfo fenceInfo {};
     fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
     fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
-    for (unsigned idx = 0; idx < myMaximumInFligthFrames; idx++) {
+    for (unsigned idx = 0; idx < myMaximumInFlightFrames; idx++) {
         VkResult result = vkCreateSemaphore(myContext.myLogicalDevice,
                                             &semaphoreInfo,
                                             nullptr,
@@ -53,22 +60,21 @@ Swapchain::RtCode Swapchain::Initialize()
             return RtCode::DRIVER_CALL_ERROR;
         }
         result =
-            vkCreateFence(myContext.myLogicalDevice, &fenceInfo, nullptr, &myInFlyFrameFence[idx]);
+            vkCreateFence(myContext.myLogicalDevice, &fenceInfo, nullptr, &myInFlyFence[idx]);
         if (result != VK_SUCCESS) {
             return RtCode::DRIVER_CALL_ERROR;
         }
     }
-    CreateSwapchain();
     return RtCode::SUCCESS;
 }
 
 Swapchain::RtCode Swapchain::Teardown()
 {
     Release();
-    for (int idx = 0; idx < myMaximumInFligthFrames; idx++) {
+    for (int idx = 0; idx < myMaximumInFlightFrames; idx++) {
         vkDestroySemaphore(myContext.myLogicalDevice, myAvailableFrameSemaphore[idx], nullptr);
         vkDestroySemaphore(myContext.myLogicalDevice, myRenderedFrameSemaphore[idx], nullptr);
-        vkDestroyFence(myContext.myLogicalDevice, myInFlyFrameFence[idx], nullptr);
+        vkDestroyFence(myContext.myLogicalDevice, myInFlyFence[idx], nullptr);
     }
     return RtCode::SUCCESS;
 }
@@ -76,6 +82,11 @@ Swapchain::RtCode Swapchain::Teardown()
 Swapchain::RtCode Swapchain::Build()
 {
     CreateImageViews();
+    CreateRenderPass();
+    CreateDepthResources();
+    CreateFrameBuffer();
+    CreateGraphicsCommandPool();
+    CreateCommandBuffers();
     return RtCode::SUCCESS;
 }
 
@@ -84,11 +95,29 @@ Swapchain::RtCode Swapchain::ReBuild()
     Release();
     CreateSwapchain();
     CreateImageViews();
+    CreateRenderPass();
+    CreateDepthResources();
+    CreateFrameBuffer();
+    CreateGraphicsCommandPool();
+    CreateCommandBuffers();
     return RtCode::SUCCESS;
 }
 
 Swapchain::RtCode Swapchain::Release()
 {
+    vkDeviceWaitIdle(myContext.myLogicalDevice);
+    vkDestroyImageView(myContext.myLogicalDevice, myDepthImageView, nullptr);
+    vkDestroyImage(myContext.myLogicalDevice, myDepthImage, nullptr);
+    vkFreeMemory(myContext.myLogicalDevice, myDepthImageMemory, nullptr);
+    for (auto* frameBuffer : mySwapchainFrameBuffers) {
+        vkDestroyFramebuffer(myContext.myLogicalDevice, frameBuffer, nullptr);
+    }
+    vkFreeCommandBuffers(myContext.myLogicalDevice,
+                         myCommandPool,
+                         myCommandBuffers.size(),
+                         myCommandBuffers.data());
+    vkDestroyCommandPool(myContext.myLogicalDevice, myCommandPool, nullptr);
+    myRenderPass.Release();
     for (auto* imageView : mySwapchainImageViews) {
         vkDestroyImageView(myContext.myLogicalDevice, imageView, nullptr);
     }
@@ -96,13 +125,105 @@ Swapchain::RtCode Swapchain::Release()
     return RtCode::SUCCESS;
 }
 
-Swapchain::RtCode Swapchain::PrepareFrame()
+Swapchain::RtCode Swapchain::PrepareFrame(Frame& aFrame)
 {
+    vkWaitForFences(myContext.myLogicalDevice,
+                    1,
+                    &myInFlyFence[myCurrentFrame],
+                    VK_TRUE,
+                    UINT64_MAX);
+    VkResult result = vkAcquireNextImageKHR(myContext.myLogicalDevice,
+                                            mySwapchain,
+                                            UINT64_MAX,
+                                            myAvailableFrameSemaphore[myCurrentFrame],
+                                            nullptr,
+                                            &myFrameIndex);
+    if (result == VK_ERROR_OUT_OF_DATE_KHR) {
+        return RtCode::OUT_OF_DATE_SWAPCHAIN;
+    }
+    if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    aFrame.myFrameIdx = myFrameIndex;
+
+    if (myInFlyFrameFence[myFrameIndex] != nullptr) {
+        vkWaitForFences(myContext.myLogicalDevice,
+                        1,
+                        &myInFlyFrameFence[myFrameIndex],
+                        VK_TRUE,
+                        UINT64_MAX);
+    }
+    myInFlyFrameFence[myFrameIndex] = myInFlyFence[myCurrentFrame];
+    aFrame.myCommandBuffer = myCommandBuffers[myFrameIndex];
+    // if flag -> 0 keep memory, otherwise VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT to return memory to cmd pool
+    vkResetCommandBuffer(aFrame.myCommandBuffer, 0);
+    VkCommandBufferBeginInfo beginInfo {};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = 0;
+    beginInfo.pInheritanceInfo = nullptr;
+
+    if (vkBeginCommandBuffer(aFrame.myCommandBuffer, &beginInfo) != VK_SUCCESS) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    std::array<VkClearValue, 2> clearValues {};
+    clearValues[0].color = { 0.0f, 0.0f, 0.0f, 1.0f };
+    clearValues[1].depthStencil = { 1.0f, 0 };
+
+    VkRenderPassBeginInfo renderPassInfo {};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassInfo.renderPass = myRenderPass;
+    renderPassInfo.framebuffer = mySwapchainFrameBuffers[myFrameIndex];
+    renderPassInfo.renderArea.offset = { 0, 0 };
+    renderPassInfo.renderArea.extent = mySwapchainExtent;
+    renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+    renderPassInfo.pClearValues = clearValues.data();
+    vkCmdBeginRenderPass(aFrame.myCommandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
     return RtCode::SUCCESS;
 }
 
-Swapchain::RtCode Swapchain::SubmitFrame()
+Swapchain::RtCode Swapchain::SubmitFrame(Frame& aFrame)
 {
+    vkCmdEndRenderPass(aFrame.myCommandBuffer);
+    if (vkEndCommandBuffer(aFrame.myCommandBuffer) != VK_SUCCESS) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    VkSemaphore          waitSemaphores[] = { myAvailableFrameSemaphore[myCurrentFrame] };
+    VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
+    VkSubmitInfo         submitInfo {};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.waitSemaphoreCount = 1;
+    submitInfo.pWaitSemaphores = waitSemaphores;
+    submitInfo.pWaitDstStageMask = waitStages;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &aFrame.myCommandBuffer;
+    VkSemaphore signalSemaphores[] = { myRenderedFrameSemaphore[myCurrentFrame] };
+    submitInfo.signalSemaphoreCount = 1;
+    submitInfo.pSignalSemaphores = signalSemaphores;
+    vkResetFences(myContext.myLogicalDevice, 1, &myInFlyFence[myCurrentFrame]);
+    VkResult result = vkQueueSubmit(myContext.myGraphicsQueue,
+                                    1,
+                                    &submitInfo,
+                                    myInFlyFence[myCurrentFrame]);
+    if (result != VK_SUCCESS) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    VkPresentInfoKHR presentInfo {};
+    presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+    presentInfo.waitSemaphoreCount = 1;
+    presentInfo.pWaitSemaphores = signalSemaphores;
+    VkSwapchainKHR swapChains[] = { mySwapchain };
+    presentInfo.swapchainCount = 1;
+    presentInfo.pSwapchains = swapChains;
+    presentInfo.pImageIndices = &myFrameIndex;
+    presentInfo.pResults = nullptr;
+    result = vkQueuePresentKHR(myContext.myPresentQueue, &presentInfo);
+    if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR) {
+        return RtCode::OUT_OF_DATE_SWAPCHAIN;
+    }
+    else if (result != VK_SUCCESS) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    myCurrentFrame = (myCurrentFrame + 1) % myMaximumInFlightFrames;
     return RtCode::SUCCESS;
 }
 
@@ -194,6 +315,127 @@ Swapchain::RtCode Swapchain::CreateImageViews()
         if (result != VK_SUCCESS) {
             return RtCode::DRIVER_CALL_ERROR;
         }
+    }
+    return RtCode::SUCCESS;
+}
+
+Swapchain::RtCode Swapchain::CreateRenderPass()
+{
+    myRenderPass.Reset();
+
+    VkAttachmentDescription colorAttachment {};
+    colorAttachment.format = mySwapchainImageFormat;
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+    VkAttachmentReference colorAttachmentRef {};
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    myRenderPass.AddColorAttachment(colorAttachment, colorAttachmentRef);
+
+    VkAttachmentDescription depthAttachment {};
+    depthAttachment.format = VkUtils::FindDepthBufferingImageFormat(myContext.myPhysicalDevice);
+    depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    depthAttachment.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+    VkAttachmentReference depthAttachmentRef {};
+    depthAttachmentRef.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+    myRenderPass.AddDepthStencilAttachment(depthAttachment, depthAttachmentRef);
+
+    if (myRenderPass.Build() != RenderPass::RtCode::SUCCESS) {
+        return Swapchain::RtCode::DRIVER_CALL_ERROR;
+    }
+    return Swapchain::RtCode::SUCCESS;
+}
+
+Swapchain::RtCode Swapchain::CreateDepthResources()
+{
+    VkFormat depthFormat = VkUtils::FindDepthBufferingImageFormat(myContext.myPhysicalDevice);
+    assert(depthFormat != VK_FORMAT_UNDEFINED);
+    auto [image, imageMemory] = VkUtils::AllocImage(myContext,
+                                                    mySwapchainExtent.width,
+                                                    mySwapchainExtent.height,
+                                                    depthFormat,
+                                                    VK_IMAGE_TILING_OPTIMAL,
+                                                    VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+                                                    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+    myDepthImage = image;
+    myDepthImageMemory = imageMemory;
+    if (!myDepthImage) {
+        return RtCode::MEMORY_ALLOCATION_ERROR;
+    }
+    myDepthImageView = VkUtils::CreateImageView(myContext.myLogicalDevice,
+                                                myDepthImage,
+                                                depthFormat,
+                                                VK_IMAGE_ASPECT_DEPTH_BIT);
+    if (!myDepthImageView) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    return RtCode::SUCCESS;
+}
+
+Swapchain::RtCode Swapchain::CreateFrameBuffer()
+{
+    mySwapchainFrameBuffers.resize(myFrameCount);
+    for (size_t i = 0; i < myFrameCount; i++) {
+        std::array<VkImageView, 2> attachments = { mySwapchainImageViews[i], myDepthImageView };
+
+        VkFramebufferCreateInfo frameBufferInfo {};
+        frameBufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        frameBufferInfo.renderPass = myRenderPass;
+        frameBufferInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
+        frameBufferInfo.pAttachments = attachments.data();
+        frameBufferInfo.width = mySwapchainExtent.width;
+        frameBufferInfo.height = mySwapchainExtent.height;
+        frameBufferInfo.layers = 1;
+
+        if (vkCreateFramebuffer(myContext.myLogicalDevice,
+                                &frameBufferInfo,
+                                nullptr,
+                                &mySwapchainFrameBuffers[i]) != VK_SUCCESS) {
+            return RtCode::DRIVER_CALL_ERROR;
+        }
+    }
+    return RtCode::SUCCESS;
+}
+
+Swapchain::RtCode Swapchain::CreateGraphicsCommandPool()
+{
+    VkUtils::QueueFamilyIndices queueFamilyIndices =
+        VkUtils::FindQueueFamilies(myContext.myPhysicalDevice, myContext.mySurface);
+
+    VkCommandPoolCreateInfo poolInfo {};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    if (vkCreateCommandPool(myContext.myLogicalDevice, &poolInfo, nullptr, &myCommandPool) !=
+        VK_SUCCESS) {
+        return RtCode::DRIVER_CALL_ERROR;
+    }
+    return RtCode::SUCCESS;
+}
+
+Swapchain::RtCode Swapchain::CreateCommandBuffers()
+{
+    myCommandBuffers.resize(myFrameCount);
+    VkCommandBufferAllocateInfo allocInfo {};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = myCommandPool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = static_cast<uint32_t>(myCommandBuffers.size());
+
+    if (vkAllocateCommandBuffers(myContext.myLogicalDevice, &allocInfo, myCommandBuffers.data()) !=
+        VK_SUCCESS) {
+        return RtCode::DRIVER_CALL_ERROR;
     }
     return RtCode::SUCCESS;
 }
--- modules/graphics/Private/Swapchain/Swapchain.hpp
+++ modules/graphics/Private/Swapchain/Swapchain.hpp
@@ -7,55 +7,95 @@
 
 #include <vector>
 
+#include <Vulkan/RenderPass/RenderPass.hpp>
 #include <Vulkan/Vulkan.hpp>
 
 namespace Netero::Gfx {
 
 class Context;
 
+struct Frame {
+    uint32_t        myFrameIdx;
+    VkCommandBuffer myCommandBuffer;
+};
+
 class Swapchain {
     public:
-    enum class RtCode { SUCCESS, DRIVER_CALL_ERROR, OUT_OF_DATE_SWAPCHAIN };
+    enum class RtCode {
+        SUCCESS,
+        DRIVER_CALL_ERROR,
+        OUT_OF_DATE_SWAPCHAIN,
+        MEMORY_ALLOCATION_ERROR
+    };
 
-    Swapchain(Context& aContext, uint32_t aMaxInFligthFrameCount);
+    Swapchain(Context& aContext, uint32_t aMaxInFlightFrameCount);
 
-    // Initialization
+#pragma region Initialization Methods
+
     RtCode Initialize();
     RtCode Teardown();
 
-    // Runtime related
+#pragma region Runtime Methods
+
     RtCode Build();
     RtCode ReBuild();
     RtCode Release();
 
+#pragma region Utility Methods
+
     VkFormat GetSwapchainImagesFormat() { return mySwapchainImageFormat; }
     unsigned GetCurrentFrameIdx() { return myCurrentFrame; }
-    RtCode   PrepareFrame();
-    RtCode   SubmitFrame();
+    uint32_t GetFrameCount() { return myFrameCount; }
+    RtCode   PrepareFrame(Frame& aFrame);
+    RtCode   SubmitFrame(Frame& aFrame);
+
+#pragma region Private Utility Methods
 
     private:
     RtCode CreateSwapchain();
     RtCode CreateImageViews();
+    RtCode CreateRenderPass();
+    RtCode CreateDepthResources();
+    RtCode CreateFrameBuffer();
+    RtCode CreateGraphicsCommandPool();
+    RtCode CreateCommandBuffers();
+
+#pragma region Private Attributes
 
     private:
-    Context&  myContext;
-    int       myCurrentFrame;
-    uint32_t  myFrameCount;
-    const int myMaximumInFligthFrames;
+    Context&   myContext;
+    RenderPass myRenderPass;
+    uint32_t   myCurrentFrame;
+    uint32_t   myFrameCount;
+    uint32_t   myFrameIndex;
+    const int  myMaximumInFlightFrames;
 
-    // Synchronization related
+#pragma region - Synchronization Attributes
+
     std::vector<VkSemaphore> myAvailableFrameSemaphore;
     std::vector<VkSemaphore> myRenderedFrameSemaphore;
+    std::vector<VkFence>     myInFlyFence;
     std::vector<VkFence>     myInFlyFrameFence;
-    std::vector<VkFence>     myRenderedFrameFence;
 
-    // Swapchain related
+#pragma region - Swapchain Attributes
+
     VkSwapchainKHR             mySwapchain;
     VkExtent2D                 mySwapchainExtent;
     std::vector<VkImage>       mySwapchainImages;
     VkFormat                   mySwapchainImageFormat;
     std::vector<VkImageView>   mySwapchainImageViews;
     std::vector<VkFramebuffer> mySwapchainFrameBuffers;
+
+#pragma region - Depth Attributes
+
+    VkImage        myDepthImage;
+    VkDeviceMemory myDepthImageMemory;
+    VkImageView    myDepthImageView;
+
+#pragma region - CommandBuffers Attributes
+
+    VkCommandPool                myCommandPool;
+    std::vector<VkCommandBuffer> myCommandBuffers;
 };
 
 } // namespace Netero::Gfx
