db2a4a9b0272e211535b966ab177edef275ed41a
Jean-Adrien Domage <jeanadrien.d@gmail.com>
2020-10-20 15:49:53 +0200

    core/ update Set unit test to improve code coverage

--- modules/Core/Tests/set_test.cpp
+++ modules/Core/Tests/set_test.cpp
@@ -7,7 +7,7 @@
 
 #include <gtest/gtest.h>
 
-TEST(NeteroCore, set_basic_usage)
+TEST(NeteroCore, Set_basic_usage)
 {
     Netero::Set<int> a { 1, 2, 3, 4, 5, 6, 7, 8 };
     Netero::Set<int> b { 6, 7, 8 };
@@ -19,4 +19,23 @@ TEST(NeteroCore, set_basic_usage)
     EXPECT_TRUE(b.IsSubsetOf(a));
     EXPECT_FALSE(e.IsSubsetOf(a));
     EXPECT_TRUE(e.InterWith(a));
+}
+
+TEST(NeteroCore, Set_empty_other_test)
+{
+    Netero::Set<int> a { 1, 2, 3, 4, 5, 6, 7, 8 };
+    Netero::Set<int> b;
+
+    EXPECT_FALSE(a.IsSubsetOf(b));
+    EXPECT_FALSE(a.InterWith(b));
+}
+
+TEST(NeteroCore, Set_inter_with_test)
+{
+    Netero::Set<int> a { 1, 2, 3, 4, 5, 6, 7, 8 };
+    Netero::Set<int> b { 4 };
+    Netero::Set<int> c { 9 };
+
+    EXPECT_TRUE(a.InterWith(b));
+    EXPECT_FALSE(a.InterWith(c));
 }
